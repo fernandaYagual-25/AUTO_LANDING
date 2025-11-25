57f368c52ec5c25dcb9a685d93d0cea271e0d77a
fernandaYagual-25 <mariafernandayagual638@gmail.com>
2025-11-25 11:24:39 -0500

    Create qaser_eporter.py

---
+++ AUTO_LANDING/qaser_eporter.py
@@ -0,0 +1,20 @@
+from qaseio import QaseApi, models
+import os
+
+QASE_TOKEN = os.getenv("QASE_TOKEN")
+PROJECT_CODE = "IQ"   # Cambia si tu código es otro
+
+api = QaseApi(token=QASE_TOKEN)
+
+def report_to_qase(case_id: int, status: bool, comment=""):
+    """Envía resultados a Qase"""
+    status_text = "passed" if status else "failed"
+
+    api.results.create(
+        code=PROJECT_CODE,
+        data=models.TestResultCreate(
+            case_id=case_id,
+            status=status_text,
+            comment=comment
+        )
+    )
